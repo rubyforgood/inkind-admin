8f6a475cd8445afb2fd9454ae45ff6d330cf7677
Meg Gutshall <meghan.gutshall@gmail.com>
2021-09-24 17:20:24 -0400

    Create static#home action scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
-Rails
-  .application
-  .routes
-  .draw do
-    devise_for :users
+Rails.application.routes.draw do
+  root to: 'static#home'
 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
