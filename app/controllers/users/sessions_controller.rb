c69a49f87a5c4d5eb0c041d143bf824669617e76
Meg Gutshall <meghan.gutshall@gmail.com>
2021-09-25 15:04:31 -0400

    Customize Devise after_sign_in_path_for method

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Users::Sessions::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  def after_sign_in_path_for(_resource)
+    if current_user.admin?
+      redirect_to '/admin/dashboard/home'
+    else
+      redirect_to :root
+    end
+  end
+end
