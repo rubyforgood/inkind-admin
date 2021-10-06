a40670708831c4ce792becc897ee5972429a4587
Garett Arrowood <garettarrowood@gmail.com>
2021-10-06 12:30:03 -0400

    Update tests

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :student do
-    name { Faker::Name.name }
+    first_name { Faker::Name.gender_neutral_first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     date_of_birth { 13.years.ago + (rand(600) * (rand > 0.5 ? -1 : 1)) }
 
