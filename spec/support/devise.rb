a830dbee96eb609ed2b037312e932edc95a84730
Meg Gutshall <meghan.gutshall@gmail.com>
2021-10-07 17:36:27 -0400

    Add Devise configs

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,3 +1,7 @@
 RSpec.configure do |config|
+  # RSpec/Devise controller test configs: https://rubydoc.info/github/heartcombo/devise/#controller-tests
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  # RSpec/Devise integration test configs: https://rubydoc.info/github/heartcombo/devise/#integration-tests
   config.include Devise::Test::IntegrationHelpers, type: :system
 end
