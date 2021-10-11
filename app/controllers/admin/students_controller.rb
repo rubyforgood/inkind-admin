f7f5c7b0096b760048c419cb876e5bf4cf9f3b5f
Meg Gutshall <meghan.gutshall@gmail.com>
2021-10-11 15:44:13 -0400

    Add admin path to students controller

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -2,25 +2,25 @@ module Admin
   class StudentsController < ApplicationController
     before_action :set_student, only: %i[show edit update destroy]
 
-    # GET /students
+    # GET /admin/students
     def index
       @students = Student.all
     end
 
-    # GET /students/1
+    # GET /admin/students/1
     def show
     end
 
-    # GET /students/new
+    # GET /admin/students/new
     def new
       @student = Student.new
     end
 
-    # GET /students/1/edit
+    # GET /admin/students/1/edit
     def edit
     end
 
-    # POST /students
+    # POST /admin/students
     def create
       @student = Student.new(student_params)
 
@@ -31,7 +31,7 @@ module Admin
       end
     end
 
-    # PATCH/PUT /students/1
+    # PATCH/PUT /admin/students/1
     def update
       if @student.update(student_params)
         redirect_to admin_student_path(@student), notice: "Student was successfully updated."
@@ -40,7 +40,7 @@ module Admin
       end
     end
 
-    # DELETE /students/1
+    # DELETE /admin/students/1
     def destroy
       @student.destroy
       redirect_to admin_students_url, notice: "Student was successfully destroyed."
