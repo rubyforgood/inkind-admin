1040d73fd28c326134e9159bf820acecb7544578
Rodolfo Santos <rodolfoasantos@gmail.com>
2021-10-12 10:01:59 -0300

    Display Formatted Student Ages & Session Dates (#49)
    * Make graphql return a DateTime type
    
    * Fix specs
    
    * Refactoring of time parser

--- app/graphql/types/meeting_duration_type.rb
+++ app/graphql/types/meeting_duration_type.rb
@@ -1,7 +1,7 @@
 module Types
   class MeetingDurationType < Base::Object
     field :id, ID, null: false
-    field :started_at, String, null: false
+    field :started_at, GraphQL::Types::ISO8601DateTime, null: false
     field :minutes, Int, null: false
   end
 end
