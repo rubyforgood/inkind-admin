b629632858daf0a370fbc2bf307cb25b3ddf55db
Rodolfo Santos <rodolfoasantos@gmail.com>
2021-10-12 12:23:34 -0300

    Edit Volunteer

--- app/controllers/admin/volunteers_controller.rb
+++ app/controllers/admin/volunteers_controller.rb
@@ -4,5 +4,22 @@ module Admin
     def index
       @volunteers = User.where(role: :volunteer)
     end
+
+    def edit
+      @volunteer = User.find(params[:id])
+    end
+
+    def update
+      @volunteer = User.find(params[:id])
+      if @volunteer.update(volunteer_params)
+        redirect_to admin_volunteer_path(@volunteer), notice: "Volunteer was successfully updated."
+      else
+        render :edit
+      end
+    end
+
+    def volunteer_params
+      params.require(:user).permit(:first_name, :last_name, :email, :phone_number)
+    end
   end
 end
