719b02b0c01457f6f8194424b9e98976b66a07db
Rodolfo Santos <rodolfoasantos@gmail.com>
2021-10-12 11:54:34 -0300

    Exclude destroy from routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get "dashboard", to: "dashboard#home"
-    resources :volunteers
+    resources :volunteers, except: :destroy
   end
 end
