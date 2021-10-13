9f7456ba19b9f2d4d616a482e8d5ee1adf7a30b4
Carlos Palhares <chjunior@gmail.com>
2021-10-13 12:04:39 -0300

    Remove unnecessary static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  root to: "static#home"
+  # App Paths
+  root to: "admin/dashboard#home"
 
+  # GraphQL
   post "/graphql", to: "graphql#execute"
-
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
 
+  # Devise Setup
   devise_scope :user do
     get "users/sign_out" => "devise/sessions#destroy"
   end
-
   devise_for :users, path: "users"
-
-  namespace :admin do
-    get "dashboard", to: "dashboard#home"
-  end
 end
