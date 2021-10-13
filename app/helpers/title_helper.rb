76bab4080d18b904e1015febe67674ffd14cc2f9
Carlos Palhares <chjunior@gmail.com>
2021-10-13 12:39:23 -0300

    Add avatar back to layout

--- app/helpers/title_helper.rb
+++ app/helpers/title_helper.rb
@@ -13,9 +13,4 @@ module TitleHelper
   def default_title
     t(:title)
   end
-
-  def current_user_avatar_url
-    gravatar_id = Digest::MD5::hexdigest(current_user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
-  end
 end
