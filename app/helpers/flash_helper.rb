434bd06e6c8313804887ec1582bb8a3989e7503b
Carlos Palhares <chjunior@gmail.com>
2021-10-13 11:49:51 -0300

    Handle alerts with bootstarp alerts

---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,13 @@
+module FlashHelper
+  CSS_CLASS = {
+    notice: "alert-info",
+    success: "alert-success",
+    error: "alert-danger",
+    alert: "alert-warning",
+  }
+  def flash_banner(level, message)
+    return unless message
+
+    content_tag(:div, message, class: ["alert", level, CSS_CLASS[level]].uniq)
+  end
+end
