760d8196f054596502646c2cef360fcc5450eb91
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-10-14 11:22:28 -0400

    Add query and tests

--- app/graphql/queries/survey_responses_query.rb
+++ app/graphql/queries/survey_responses_query.rb
@@ -7,10 +7,19 @@ module Queries
         [Types::SurveyResponseType],
         null: true,
         description: "All survey_responses associated with signed in volunteer"
+
+      field :student_survey_responses, [Types::SurveyResponseType], null: true do
+        description "All survey_responses for a specific student associated with signed in volunteer"
+        argument :id, GraphQL::Types::ID, required: true
+      end
     end
 
     def survey_responses
       context[:current_user]&.survey_responses
+    end
+
+    def student_survey_responses(id:)
+      context[:current_user]&.survey_responses&.where(student_id: id)
     end
   end
 end
--- spec/graphql/queries/survey_responses_spec.rb
+++ spec/graphql/queries/survey_responses_spec.rb
@@ -22,6 +22,26 @@ module Queries
       GQL
     end
 
+    def student_query(student)
+      <<~GQL
+        query {
+          studentSurveyResponses(id: #{student.id}) {
+            student {
+              name
+            }
+            volunteer {
+              id
+              name
+            }
+            meetingDuration {
+              minutes
+              startedAt
+            }
+          }
+        }
+      GQL
+    end
+
     def create_response(volunteer, student, minutes, started_at)
       create(
         :survey_response,
@@ -31,7 +51,7 @@ module Queries
       )
     end
 
-    describe ".resolve" do
+    describe "all surveys .resolve" do
       let(:volunteer) { create(:user, :volunteer) }
 
       before do
@@ -69,6 +89,46 @@ module Queries
 
       it "returns nothing when not signed in", :aggregate_failures do
         post "/graphql", params: {query: query}
+        expect(response).to be_successful
+
+        json = JSON.parse(response.body)
+        data = json["data"]["survey_responses"]
+
+        expect(data).to be_nil
+      end
+    end
+
+    describe "student surveys .resolve" do
+      let(:volunteer) { create(:user, :volunteer) }
+
+      before do
+        adam = create(:student, first_name: "Adam", last_name: "Ruby")
+        gia = create(:student, first_name: "Gia", last_name: "ForGood")
+        travel_to(yesterday) do
+          create_response(volunteer, adam, 42, Time.zone.now - 6.hours)
+          create_response(volunteer, gia, 46, Time.zone.now - 4.hours)
+        end
+      end
+
+      context "when signed in" do
+        it "returns all survey_responses belonging to user and student", :aggregate_failures do
+          sign_in(volunteer)
+          adam = Student.find_by(first_name: "Adam", last_name: "Ruby")
+
+          post "/graphql", params: { query: student_query(adam) }
+
+          expect(response).to be_successful
+          json = JSON.parse(response.body)
+          data = json["data"]["studentSurveyResponses"]
+
+          expect(data.size).to eq(1)
+          expect(data[0]["student"]["name"]).to eq("Adam Ruby")
+        end
+      end
+
+      it "returns nothing when not signed in", :aggregate_failures do
+        adam = Student.find_by(first_name: "Adam", last_name: "Ruby")
+        post "/graphql", params: { query: student_query(adam) }
         expect(response).to be_successful
 
         json = JSON.parse(response.body)
