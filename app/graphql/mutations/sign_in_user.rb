b10b4e8d0e822c759d05265f8b65bf02d2bf0c59
Rimenes Ribeiro <rima@rimenesribeiro.com>
2021-10-14 15:12:54 -0300

    Do not allow inactive users to login

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -16,6 +16,8 @@ module Mutations
       # ensures we have the correct user
       raise GraphQL::ExecutionError, "user with email does not exist" unless user
 
+      raise GraphQL::ExecutionError, "account associated with email is no longer active" if user.inactive?
+
       raise GraphQL::ExecutionError, "invalid password for user" unless user.valid_password?(credentials[:password])
 
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
