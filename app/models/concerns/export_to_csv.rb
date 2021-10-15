67d0f544711b23340a3cc69c22793e91f57217d5
Felipe Casadei <felipe.casadei@gmail.com>
2021-10-15 12:50:01 -0300

    Export students (#98)
    * Add a concern to export aa data collection as a csv
    
    * Supports csv format when listing the students
    
    * Add a button to export students data
    
    * Fix lint offenses

---
+++ app/models/concerns/export_to_csv.rb
@@ -0,0 +1,19 @@
+module ExportToCsv
+  extend ActiveSupport::Concern
+
+  included do
+    def self.export_to_csv(collection, columns: column_names, headers: columns)
+      CSV.generate(headers: true, force_quotes: true) do |csv|
+        csv << headers
+
+        collection.each do |obj|
+          csv << if obj.respond_to?(:to_csv)
+            obj.to_csv
+          else
+            columns.map { |column| column.split(".").inject(obj, :send) }
+          end
+        end
+      end
+    end
+  end
+end
