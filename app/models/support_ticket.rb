a82a2b66c7b280b34ea1ea606a0925552ce8a164
Felipe Casadei <felipe.casadei@gmail.com>
2021-10-14 23:58:38 -0300

    Add a method to close the support ticket

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -7,7 +7,13 @@ class SupportTicket < ApplicationRecord
   belongs_to :survey_response, foreign_key: :survey_response_id,
                                optional: true
 
-  enum status: {active: 0, closed: 1}
+  enum status: { active: 0, closed: 1 }
 
   validates :description, presence: true
+
+  def close!(closer)
+    return false if closed?
+
+    update!(status: :closed, closer: closer, closed_at: Time.current)
+  end
 end
--- spec/models/support_ticket_spec.rb
+++ spec/models/support_ticket_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe SupportTicket, type: :model do
   it "has valid factory" do
     expect(create(:support_ticket)).to be_valid
   end
+
+  context "#close!(user)" do
+    let(:user) { create(:user) }
+    subject(:support_ticket) { create(:support_ticket) }
+
+    it "returns false if the ticket is already closed" do
+      expect(build(:support_ticket, status: :closed).close!(user)).to be_falsey
+    end
+
+    it "udpates ticket's status to closed" do
+      support_ticket.close!(user)
+      expect(support_ticket).to be_closed
+    end
+
+    it "udpates ticket's closer" do
+      support_ticket.close!(user)
+      expect(support_ticket.closer).to eq user
+    end
+
+    it "udpates ticket's closed_at" do
+      freeze_time do
+        support_ticket.close!(user)
+        expect(support_ticket.closed_at).to eq Time.current
+      end
+    end
+  end
 end
