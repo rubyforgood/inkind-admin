1a4b0528a41384d744b7d344038cb225bbc96130
Felipe Casadei <felipe.casadei@gmail.com>
2021-10-15 13:39:43 -0300

    Add testing coverage

--- spec/requests/volunteers_spec.rb
+++ spec/requests/volunteers_spec.rb
@@ -13,6 +13,36 @@ RSpec.describe "/admin/volunteers", type: :request do
     attributes_for(:user, email: "")
   end
 
+  describe "GET /index" do
+    context "with format csv" do
+      it "generates the csv file" do
+        travel_to(Date.new(2021, 10, 14)) do
+          volunteer1 = create(:volunteer, first_name: "Zion", last_name: "Vandervort", email: "volunteer-1@example.edu", status: :inactive)
+          create(:support_ticket, requestor: volunteer1)
+          create(:support_ticket, requestor: volunteer1)
+
+          volunteer2 = create(:volunteer, first_name: "Sutton", last_name: "Faddel", email: "volunteer-2@example.edu")
+
+          create(:survey_response, volunteer: volunteer2, meeting_duration: build(:meeting_duration))
+          create(:survey_response, volunteer: volunteer2, meeting_duration: build(:meeting_duration))
+          create(:survey_response, volunteer: volunteer2, meeting_duration: nil)
+
+          create(:support_ticket, requestor: volunteer2)
+
+          get admin_volunteers_url, params: { format: :csv }
+
+          expect(response.header["Content-Type"]).to include "text/csv"
+          expect(response.headers["Content-Disposition"]).to include "attachment; filename=\"volunteers-2021-10-14.csv\""
+          expect(response.body).to eq <<~CSV
+            "Name","Email","Phone","Status","Last Seeen","Total Surveys Completed","Total Support Tickets Created"
+            "Zion Vandervort","volunteer-1@example.edu","","inactive","","0","2"
+            "Sutton Faddel","volunteer-2@example.edu","","active","2021-10-14 00:00:00 UTC","2","1"
+          CSV
+        end
+      end
+    end
+  end
+
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a new Volunteer" do
