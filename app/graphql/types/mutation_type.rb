b1cd2145fb0ec6d594d01ff78f07694c29637d19
Gabriel Pereira <gmfvpereira@gmail.com>
2021-10-15 12:06:54 -0300

    Save question responses

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Base::Object
     field :sign_in_user, mutation: Mutations::SignInUser
     field :create_survey_response, mutation: Mutations::CreateSurveyResponse
+    field :create_survey_question_response, mutation: Mutations::CreateSurveyQuestionResponse
   end
 end
