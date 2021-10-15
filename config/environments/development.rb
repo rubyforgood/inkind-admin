9d858c0540f3e000a7d9715672584df41cc19957
Carlos Palhares <chjunior@gmail.com>
2021-10-15 18:13:27 -0300

    Fix lint violation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.preview_path = Rails.root.join('spec')
+  config.action_mailer.preview_path = Rails.root.join("spec")
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
