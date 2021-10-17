2becb9fe3f7148657e673d1522fbd8dbc588d7c4
Garett Arrowood <garettarrowood@gmail.com>
2021-10-16 20:23:19 -0400

    Fix helper tests
    TODO: Evaluate if this is a good test

--- spec/helpers/title_helper_spec.rb
+++ spec/helpers/title_helper_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe TitleHelper, type: :helper do
   describe "#page_title" do
     it "is the title content and the default title" do
       helper.content_for(:title) { "Users" }
-      expect(helper.page_title).to eql "Users | InKind Admin"
+      expect(helper.page_title).to eql "Users | INKIND"
     end
 
     it "is the default title when no title content exists" do
-      expect(helper.page_title).to eql "InKind Admin"
+      expect(helper.page_title).to eql "INKIND"
     end
   end
 end
