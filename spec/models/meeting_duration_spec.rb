c87cda1330f7afc3dc3be3de0e2839f68176f6ea
Stephen Crane <scrane88@gmail.com>
2021-10-27 09:11:58 -0400

    Update specs

--- spec/models/meeting_duration_spec.rb
+++ spec/models/meeting_duration_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe MeetingDuration, type: :model do
     expect(create(:meeting_duration)).to be_valid
   end
 
-  it "marks status of survey response to upon creation" do
+  context "associations" do
+    it { is_expected.to belong_to(:survey_response) }
+  end
+
+  context "validations" do
+    it { is_expected.to validate_presence_of(:minutes) }
+    it { is_expected.to validate_presence_of(:started_at) }
+    it { is_expected.to validate_presence_of(:survey_response) }
+  end
+
+  it "marks status of survey response to 'complete' upon creation" do
     survey_response = create(:survey_response, status: :incomplete)
 
     create(:meeting_duration, survey_response: survey_response)
