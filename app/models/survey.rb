c6588a8d336205b7aa9e115abc9067f9726fb4d5
Kelly Ryan <kellyeryan@gmail.com>
2021-10-27 14:46:23 -0400

    Add url for nav, add relationship to model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ApplicationRecord
   has_many :questions, class_name: "SurveyQuestion", dependent: :destroy
+  has_many :responses, class_name: "SurveyResponse", dependent: :destroy
 
   validates :name, presence: true
 end
