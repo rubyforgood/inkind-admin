ac2a3e8fd37a4b06bb80c41a02852aacdf9bbe71
Anna Nowak <matisnape@wp.pl>
2021-10-30 22:26:40 +0200

    Make paginator more generic

--- app/reflexes/volunteers_table_reflex.rb
+++ app/reflexes/volunteers_table_reflex.rb
@@ -18,6 +18,7 @@ class VolunteersTableReflex < ApplicationReflex
 
   def update_client
     prepare_variables
+    @nav = "Volunteers"
 
     volunteers = User.where(role: :volunteer).order("#{@order_by} #{@direction}")
     @pagy, @volunteers = pagy(volunteers, page: @page)
@@ -27,8 +28,19 @@ class VolunteersTableReflex < ApplicationReflex
       direction: @direction,
       page: @page,
       pagy: @pagy,
+      nav: @nav,
       volunteers: @volunteers
     }
+
+    # uri = URI.parse([request.base_url, request.path].join)
+    # uri.query = assigns.except(:volunteers, :pagy).to_query
+
+    # morph :nothing
+
+    # cable_ready
+    #   .inner_html(selector: "#volunteers", html: render(partial: "admin/volunteers/volunteers_table", assigns: assigns))
+    #   .push_state(url: uri.to_s)
+    #   .broadcast
 
     morph "#volunteers", render(partial: "admin/volunteers/volunteers_table", assigns: assigns)
     morph "#paginator", render(partial: "admin/shared/paginator", assigns: assigns)
