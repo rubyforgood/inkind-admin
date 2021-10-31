8bc44bc25b44670c3ae6211abd1e9ef72ac962f1
Garett Arrowood <garettarrowood@gmail.com>
2021-10-31 13:33:20 -0400

    Date representations and touchups (#138)
    * Create Survey with association & not hard-coded id
    
    * Unify date representations in table views
    
    * When new surveys are eventually created, they need a creator_id
    
    * A valid survey factory means it needs a creator

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,8 +1,9 @@
 class Survey < ApplicationRecord
+  belongs_to :creator, class_name: "User"
   has_many :questions, class_name: "SurveyQuestion", dependent: :destroy
   has_many :responses, class_name: "SurveyResponse", dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, :creator_id, presence: true
 
   enum status: {inactive: 0, active: 1}
 
