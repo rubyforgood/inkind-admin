8c11cdf5ec22c950dd0d4c36d1bb69eb950a6cc0
Garett Arrowood <garettarrowood@gmail.com>
2021-10-31 20:32:28 -0400

    Refactor away admin request spec that always hangs

--- spec/requests/admin_users_spec.rb
+++ spec/requests/admin_users_spec.rb
@@ -1,49 +1,28 @@
 require "rails_helper"
 
 RSpec.describe "/admin/admin_users", type: :request do
-  before(:each) do
-    sign_in create(:user, role: :admin)
-  end
+  before(:each) { sign_in create(:user, role: :admin) }
 
-  let(:valid_attributes) do
-    attributes_for(:user)
-  end
-
-  let(:invalid_attributes) do
-    attributes_for(:user, email: "")
-  end
-
-  describe "POST /create" do
+  describe "POST #create" do
     context "with valid parameters" do
       it "creates a new admin user" do
         expect do
-          post admin_admin_users_path, params: {user: valid_attributes}
+          post admin_admin_users_path, params: {user: attributes_for(:user)}
         end.to change(User.where(role: :admin), :count).by(1)
       end
 
       it "redirects to the created admin user" do
-        post admin_admin_users_url, params: {user: valid_attributes}
+        post admin_admin_users_url, params: {user: attributes_for(:user)}
         expect(response).to redirect_to(admin_admin_users_url)
       end
     end
+  end
 
-    context "with invalid parameters" do
-      it "does not create a new admin user" do
-        expect do
-          post admin_admin_users_url, params: {user: invalid_attributes}
-        end.to change(User.where(role: :admin), :count).by(0)
-      end
-
-      it "renders a successful response" do
-        post admin_admin_users_url, params: {user: invalid_attributes}
-        expect(response).to be_successful
-      end
-    end
-
+  describe "PATCH #update" do
     context "with invalid parameters" do
       it "renders a successful response" do
-        admin_user = User.create! valid_attributes
-        patch admin_admin_user_url(admin_user), params: {user: invalid_attributes}
+        admin_user = User.create!(attributes_for(:user))
+        patch admin_admin_user_url(admin_user), params: {user: attributes_for(:user, email: "")}
         expect(response).to be_successful
       end
     end
