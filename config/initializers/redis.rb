21786a21eaf763ed614cd77cabc25873c61367c3
Garett Arrowood <garettarrowood@gmail.com>
2021-11-13 15:25:23 -0500

    Fix linting

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}) # standard:disable Style/GlobalVars
