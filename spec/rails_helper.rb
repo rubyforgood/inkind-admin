2a99019d895c9b7f10a2c5f9629216503820d6d0
goromlagche <mrinmoy.das91@gmail.com>
2021-11-21 02:52:49 +0530

    [ISSUE-144] add specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 require "capybara/rspec"
 require "devise"
+require "stimulus_reflex_testing/rspec"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
 
