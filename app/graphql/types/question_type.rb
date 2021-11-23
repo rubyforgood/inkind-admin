a11d32c1c0f87cd371d7d8e99ab364ddbfc7d454
Garett Arrowood <garettarrowood@gmail.com>
2021-11-23 13:33:22 -0500

    Add heading & description fields to survey questions

--- app/graphql/types/question_type.rb
+++ app/graphql/types/question_type.rb
@@ -2,7 +2,9 @@ module Types
   class QuestionType < Base::Object
     field :id, ID, null: false
     field :type, String, null: false
+    field :heading, String, null: true
     field :prompt, String, null: false
+    field :description, String, null: true
     field :options, [Types::QuestionOptionType], null: true
   end
 end
