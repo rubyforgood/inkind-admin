2f908f5c412b0b348cb12aace69bb4fef0f0bd20
Garett Arrowood <garettarrowood@gmail.com>
2021-11-25 11:49:01 -0500

    Upsert instead of creating question responses (#154)

--- app/models/survey_question_response.rb
+++ app/models/survey_question_response.rb
@@ -3,4 +3,6 @@ class SurveyQuestionResponse < ApplicationRecord
                         foreign_key: :survey_response_id
   belongs_to :question, class_name: "SurveyQuestion",
                         foreign_key: :survey_question_id
+  has_many :option_responses, class_name: "SurveyQuestionOptionResponse",
+                              foreign_key: :survey_question_response_id
 end
