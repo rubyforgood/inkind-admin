e433ff93da8912f99c45b2b1daa54a15e9889e08
Garett Arrowood <garettarrowood@gmail.com>
2021-12-05 15:01:22 -0500

    Add sorting to Tickets table, adding status sorting to students (#170)
    And add tests

---
+++ spec/reflex/students_table_reflex_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative "./reflex_helper"
+
+RSpec.describe StudentsTableReflex, type: :reflex do
+  let(:reflex) { build_reflex(url: admin_students_path) }
+
+  describe "#sort" do
+    before do
+      2.times do
+        create(:student, status: :active)
+        create(:student, status: :inactive)
+      end
+    end
+
+    it_behaves_like "sortable table", "first_name", Student.all, "admin/students/_students_table"
+    it_behaves_like "sortable table", "last_name", Student.all, "admin/students/_students_table"
+    it_behaves_like "sortable table", "guardian_name", Student.all, "admin/students/_students_table"
+    it_behaves_like "sortable table", "status", Student.all, "admin/students/_students_table"
+  end
+end
