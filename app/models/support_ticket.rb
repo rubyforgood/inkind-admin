20374d797ef24ea34b2a601c2507bc20a59e6a44
goromlagche <mrinmoy.das91@gmail.com>
2021-12-24 20:05:29 +0530

    [ISSUE-157] fixes comments for PR  181

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -15,10 +15,10 @@ class SupportTicket < ApplicationRecord
   enum status: {active: 0, closed: 1}
   enum category: {survey_response: 0, admin: 1, contact_info: 2}
 
-  def close!(closer)
+  def close!(closer, closer_notes: nil)
     return false if closed?
 
-    update!(status: :closed, closer: closer, closed_at: Time.current)
+    update!(status: :closed, closer: closer, closer_notes: closer_notes, closed_at: Time.current)
   end
 
   def category_s
