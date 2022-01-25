32bee3b495bb6fc1e42463c69b671b11c7b9d568
Sean Marcia <smarcia@gmail.com>
2022-01-24 23:43:35 -0500

    staging

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://inkind-volunteering.herokuapp.com"
     resource "/graphql",
       headers: :any,
       methods: %i[get post options],
